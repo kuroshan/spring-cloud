fca7985cac23a1f6f471cfb0a3cd06ae322e8725
Americo Sierra <eltrogloditakia@gmail.com>
2020-02-05 00:37:12 -0500

    init

---
+++ config-server/src/test/java/com/kuroshan/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuroshan.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
